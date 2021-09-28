c002a53e7e748f97ba417df2fe378a4f4f40b3ba
NanditaD <bubble8773@yahoo.com>
2021-09-28 23:22:07 +0530

    basic Input is done

--- Source/FlyingGame/FlyingPlayerCharacter.cpp
+++ Source/FlyingGame/FlyingPlayerCharacter.cpp
@@ -18,17 +18,89 @@ void AFlyingPlayerCharacter::BeginPlay()
 	
 }
 
+void AFlyingPlayerCharacter::UpdateKeyPitch(float rate)
+{
+	if (FMath::Abs(rate) > 0.2f)
+		UpdatePitch(rate * 2.0f);
+}
+
+void AFlyingPlayerCharacter::UpdateKeyRoll(float rate)
+{
+	if (FMath::Abs(rate) > 0.2f)
+		UpdateRoll(rate * 2.0f);
+}
+
+void AFlyingPlayerCharacter::MouseXInputEvent(float amount)
+{
+	UpdatePitch(amount);
+}
+
+void AFlyingPlayerCharacter::MouseYInputEvent(float amount)
+{
+	UpdateRoll(amount);
+}
+
+
+void AFlyingPlayerCharacter::UpdatePitch(float rate)
+{	
+	bIsPitchKeyPressed = FMath::Abs(rate) > 0.0f;
+	pitchSpeed = FMath::FInterpTo(pitchSpeed, rate * pitchRateMultiplier, GetWorld()->GetDeltaSeconds(), 2.0f);
+}
+
+void AFlyingPlayerCharacter::UpdateRoll(float rate)
+{
+	bIsRollKeyPressed = FMath::Abs(rate) > 0.0f;
+
+	if (bIsPitchKeyPressed && !bIsRollKeyPressed) return;
+
+	float targetSpeed = bIsRollKeyPressed ? (rate * rollRateMultiplier) : (GetActorRotation().Roll * -2.0f);
+
+	rollSpeed = FMath::FInterpTo(rollSpeed, targetSpeed, GetWorld()->GetDeltaSeconds(), 2.0f);
+}
+
 // Called every frame
 void AFlyingPlayerCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+   Super::Tick(DeltaTime);
+   //Movement or Thrust
+   
+   float newForwardSpeed = forwardSpeed + (-GetActorRotation().Pitch * Acceleration* DeltaTime);
+   forwardSpeed = FMath::Clamp(newForwardSpeed, minSpeed, maxSpeed);
 
+   AddActorLocalOffset(FVector(forwardSpeed * DeltaTime, 0.0f, 0.0f), true);
+   //Rotation
+   // FRotator(
+	  // float InPitch,
+	  // float InYaw,
+	  // float InRoll
+	  // )
+   FRotator nR(pitchSpeed * DeltaTime, yawSpeed * DeltaTime, rollSpeed * DeltaTime);
+   AddActorLocalRotation(nR);
+}
+
+void AFlyingPlayerCharacter::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, 
+	FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, 
+		HitLocation, HitNormal, NormalImpulse, Hit);
+	SetActorRotation(FQuat::Slerp(GetActorRotation().Quaternion(), HitNormal.ToOrientationQuat(),
+		0.025f));
+	//Deflect
+	forwardSpeed = FMath::FInterpTo(forwardSpeed, minSpeed, GetWorld()->GetDeltaSeconds(), 5.0f);
+
+	//ToDo reduce Health 
+	//Add particle effect
 }
 
 // Called to bind functionality to input
 void AFlyingPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Turn", this, &AFlyingPlayerCharacter::MouseXInputEvent);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AFlyingPlayerCharacter::UpdateKeyRoll);
+	PlayerInputComponent->BindAxis("LookUp", this, &AFlyingPlayerCharacter::MouseYInputEvent);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AFlyingPlayerCharacter::UpdateKeyPitch);
 
 }
 
--- Source/FlyingGame/FlyingPlayerCharacter.h
+++ Source/FlyingGame/FlyingPlayerCharacter.h
@@ -15,17 +15,26 @@ public:
 	// Sets default values for this character's properties
 	AFlyingPlayerCharacter();
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = "Flight")
 		float Acceleration = 30.0f;
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = "Flight")
 		float minSpeed = 500.0f;
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = "Flight")
 		float maxSpeed = 4000.0f;
 
-	float forwardSpeed; 
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float forwardSpeed = 500.0f; 
 	float yawSpeed; 
 	float pitchSpeed;
 	float rollSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float rollRateMultiplier = 200;
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float pitchRateMultiplier = 200;
+
+	bool bIsPitchKeyPressed = false;
+	bool bIsRollKeyPressed = false;
 
 protected:
 	// Called when the game starts or when spawned
@@ -34,8 +43,8 @@ protected:
 	void UpdateKeyPitch(float rate);
 	void UpdateKeyRoll(float rate);
 
-	void mouseXInputEvent(float amount);
-	void mouseYInputEvent(float amount);
+	void MouseXInputEvent(float amount);
+	void MouseYInputEvent(float amount);
 
 	//roatation
 	void UpdatePitch(float rate);
@@ -43,6 +52,19 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	// Called when a collision Occurs
+	virtual void NotifyHit
+	(
+		class UPrimitiveComponent* MyComp,
+		AActor* Other,
+		class UPrimitiveComponent* OtherComp,
+		bool bSelfMoved,
+		FVector HitLocation,
+		FVector HitNormal,
+		FVector NormalImpulse,
+		const FHitResult& Hit
+	) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
